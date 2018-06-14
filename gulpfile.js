2b76f4269960469cbe516a4dac2b1fd1e464c667
Vernon Mullen <vernonmu@gmail.com>
2018-06-13 20:28:21 -0500

    added new gulp to minifycss and create dist.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
 'use strict';
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-const nodemon = require('gulp-nodemon')
+const nodemon = require('gulp-nodemon');
+const useref = require('gulp-useref');
+const minifyCss = require('gulp-minify-css');
+const gulpif = require('gulp-if');
+
 
 gulp.task('sass', () => {
   console.log('let us gulp!');
@@ -21,5 +25,19 @@ gulp.task('start', () => {
     ext: 'js html'
   })
 })
+
+gulp.task('copy-html-files', function() {
+  var stream = gulp.src('./public/*.html')
+    .pipe(gulp.dest('./dist/'));
+  return stream;
+});
+
+gulp.task('css-files', function () {
+    var stream = gulp.src('./public/index.html')
+    .pipe(useref()) //take a streem from index.html comment
+      .pipe(gulpif('*.css', minifyCss())) // if .css file, minify
+      .pipe(gulpif('*.css', gulp.dest('./dist'))); // copy to dist
+    return stream;
+});
 
 gulp.task('default', ['sass', 'start', 'sass:watch']);
