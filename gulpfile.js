a1dd1ef3373c5b0102c92f57b39782d17d0eaea9
Vernon Mullen <vernonmu@gmail.com>
2018-06-13 22:10:08 -0500

    removed bower. added gulp for js concats and dist folder builds.

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,12 @@ const nodemon = require('gulp-nodemon');
 const useref = require('gulp-useref');
 const minifyCss = require('gulp-minify-css');
 const gulpif = require('gulp-if');
+const wiredep = require('wiredep').stream;
+const uglify = require('gulp-uglify');
+const ngAnnotate = require('gulp-ng-annotate');
+const clean = require('gulp-clean');
+const concat = require('gulp-concat');
 const runSequence = require('run-sequence');
-
 
 gulp.task('sass', () => {
   console.log('let us gulp!');
@@ -29,23 +33,67 @@ gulp.task('start', () => {
 
 gulp.task('copy-html-files', function() {
   var stream = gulp.src('./public/*.html')
-    .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest('./dist'));
   return stream;
 });
 
+gulp.task('copy-images', () => {
+  return gulp.src('./public/images/*.{gif,jpg,png,svg}')
+    .pipe(gulp.dest('./dist/images'));
+})
+
 gulp.task('css-files', function () {
     var stream = gulp.src('./public/index.html')
-    .pipe(useref()) //take a streem from index.html comment
-      .pipe(gulpif('*.css', minifyCss())) // if .css file, minify
-      .pipe(gulpif('*.css', gulp.dest('./dist'))); // copy to dist
+    .pipe(useref())
+      .pipe(gulpif('*.css', minifyCss()))
+      .pipe(gulpif('*.css', gulp.dest('./dist')));
     return stream;
 });
 
-gulp.task('default', ['sass', 'start', 'sass:watch']);
+gulp.task('bower-files', function() {
+  var stream = gulp.src('./public/index.html')
+    .pipe(wiredep({
+      directory: 'bower_components'
+    }))
+    .pipe(useref())
+    .pipe(gulpif('*.js', ngAnnotate()))
+    .pipe(gulpif('*.js', uglify()))
+    .pipe(gulpif('*.js', gulp.dest('./dist')));
+  return stream;
+})
+
+gulp.task('js-dev', () => {
+  return gulp.src('./public/**/*js')
+    .pipe(concat('whills.js'))
+    .pipe(gulp.dest('./public'))
+    console.log('done js-ing for development');
+})
+
+gulp.task('js-dev:watch', () => {
+  gulp.watch('./public/*.js', ['js-dev']);
+})
+
+gulp.task('js-files', () => {
+  return gulp.src('./public/**/*js')
+    .pipe(concat('whills.js'))
+    .pipe(gulp.dest('./dist'))
+});
+
+gulp.task('default', ['sass', 'js-dev', 'start', 'sass:watch']);
+
+
 gulp.task ('build', function(callback){
   runSequence(
     'css-files',
+    // 'bower-files',
+    'js-files',
     'copy-html-files',
     callback
   );
 });
+
+gulp.task('clean', function() {
+  var stream = gulp.src('./dist', {read: false})
+    .pipe(clean());
+  return stream
+})
---
+++ public/whills.js
@@ -0,0 +1,9 @@
+var app = angular.module('portfolio', ['duScroll', 'ngAnimate'])
+
+app.controller('mainCtrl', function($scope, mainSrv) {
+  console.log('Please enjoy your stay.');
+})
+
+app.service('mainSrv', function() {
+  console.log('Hello! Thank you for visiting ...');
+})
