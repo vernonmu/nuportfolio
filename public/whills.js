40f874f130234c0d00448c9714a6be6cfdfe01fe
Vernon Mullen <vernonmu@gmail.com>
2018-06-26 14:17:27 -0500

    added print page for small project.

--- public/whills.js
+++ public/whills.js
@@ -17,3 +17,13 @@ app.controller('mainCtrl', function($scope, mainSrv) {
 app.service('mainSrv', function() {
   console.log('Hello! Thank you for visiting ...');
 })
+
+var app = angular.module('portfolio', ['duScroll', 'ngAnimate'])
+
+app.controller('mainCtrl', function($scope, mainSrv) {
+  console.log('Please enjoy your stay.');
+})
+
+app.service('mainSrv', function() {
+  console.log('Hello! Thank you for visiting ...');
+})
